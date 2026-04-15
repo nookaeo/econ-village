4cdcdd6e8427e3fa389c7251255940f8dbd7efbe
nookaeo <jeansock.curing183@passinbox.com>
2026-04-15 17:06:13 +0700

    feat: add grow up system for agent

--- scripts/entities/agent/villagent.gd
+++ scripts/entities/agent/villagent.gd
@@ -1,25 +1,31 @@
 extends CharacterBody2D
 class_name Villagent
 
+const rate :float = 1.0
+
 @export var tile_map :TileMapLayer
 @export var genetics :Genetics
 @export var passive_energy_multiplier :float = 10
 @export var energy_drain_multiplier :float = 0.5
 @onready var sprite :Sprite2D = %Sprite
 
+
 var sex :String
 var lifespan :int
+var kid_phase :float
 var max_strength :float 
 var max_active_energy :float
 var max_passive_energy :float
-var base_speed :float
-var energy_drain_rate :float
+
+
+var sum_sry_gene_power
 
 var age :int = 0
-var strength :float = 0
-var active_energy :float = 0
-var passive_energy :float = 0
-
+var strength :float = 0.0
+var active_energy :float = 0.0
+var passive_energy :float = 0.0
+var energy_drain_rate :float = 0.0
+var base_speed :float = 0.0
 
 var target_position :Vector2 = Vector2.ZERO
 var is_moving: bool = false
@@ -28,9 +34,12 @@ var is_moving: bool = false
 func _ready() -> void:
 	var current_tile := tile_map.local_to_map(global_position)
 	global_position = tile_map.map_to_local(current_tile)
-	stats_init()
+	
 	CoreSignal.birth.emit(self, lifespan)
-	print(sex," lifespan:",lifespan," max_strength:",max_strength," max_active_energy:",max_active_energy," max_passive_energy:",max_passive_energy," energy_drain_rate:",energy_drain_rate," base_speed:",base_speed)
+	CoreSignal.day_pass.connect(_aging)
+	_base_stats_init()
+	_stats_calculate()
+	_dump_stats()
 	
 func _process(_delta: float) -> void:
 	pass
@@ -40,46 +49,83 @@ func _physics_process(delta :float) -> void:
 		global_position = global_position.move_toward(target_position, base_speed * delta)
 		if global_position == target_position:
 			is_moving = false
-
+			
+func _input(event) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
+		var clicked_tile = tile_map.local_to_map(get_global_mouse_position())
+		_move_to_grid(clicked_tile)
 #///////////////////////////////////////////////////////////////////////////#
 
-func stats_init() -> void:
-	var status_multiplier :float
-	var strength_base :float = genetics.sex.first.strength_base + genetics.sex.second.strength_base
-	var sry_gene_power :float = genetics.sex.first.sry_gene_power + genetics.sex.second.sry_gene_power
-	var lifespan_base :int = genetics.sex.first.lifespan + genetics.sex.second.lifespan
+func _base_stats_init() -> void:
+	var status_average_index :float
+	var sum_strength_base :float = genetics.sex.first.strength_base + genetics.sex.second.strength_base
+	sum_sry_gene_power= genetics.sex.first.sry_gene_power + genetics.sex.second.sry_gene_power
+	var base_lifespan :int = genetics.sex.first.lifespan + genetics.sex.second.lifespan
+	var base_kid_phase :int = genetics.sex.first.kid_phase + genetics.sex.second.kid_phase
 	
 	if genetics.sex.first.sry_gene_power + genetics.sex.second.sry_gene_power > 0 :
-		status_multiplier = 1
+		status_average_index = 1
 		sex = "male"
 		if genetics.sex.first.sry_gene_power > 0:
 			sprite.texture = genetics.sex.first.kid_texture
 		else:
 			sprite.texture = genetics.sex.second.kid_texture
 	else:
-		status_multiplier = 0.5
+		status_average_index = 0.5
 		sex = "female"
 		sprite.texture = genetics.sex.first.kid_texture
 		
-	lifespan = int(lifespan_base * (1 - (sry_gene_power * 0.25)) * status_multiplier)
-	max_strength = strength_base * (1 + sry_gene_power) * status_multiplier
-	max_active_energy = max_strength
-	max_passive_energy = floorf(max_strength * passive_energy_multiplier)
-	energy_drain_rate = floorf(max_strength * energy_drain_multiplier)
-	base_speed = clamp(floorf(400 - ((max_strength - 200) ** 2) / 200),0,500)
+	lifespan = int(base_lifespan * (rate - (sum_sry_gene_power * 0.25)) * status_average_index)
+	kid_phase = int(base_kid_phase * status_average_index)
+	max_strength =sum_strength_base * (rate + sum_sry_gene_power) * status_average_index
 	
 	
-func move_to_grid(tile_coords :Vector2i) -> void:
+func _stats_calculate() -> void:
+	strength = floorf(max_strength * (0.1 + (age / kid_phase) * 0.9)) 
+	energy_drain_rate = floorf(strength * energy_drain_multiplier)
+	base_speed = floorf(clamp(floorf(500 - ((strength - 200) ** 2) / 90),0,500))
+	max_active_energy = strength
+	max_passive_energy = floorf(strength * passive_energy_multiplier)
+
+func _move_to_grid(tile_coords :Vector2i) -> void:
 	target_position = tile_map.map_to_local(tile_coords)
 	is_moving = true
 	
-func _input(event) -> void:
-	if event is InputEventMouseButton and event.pressed:
-		var clicked_tile = tile_map.local_to_map(get_global_mouse_position())
-		move_to_grid(clicked_tile)
+
 		
 func die() -> void:
 	#Inherite properties here
 	#Drop items here
- 
 	self.queue_free()
+	
+func _aging() -> void:
+	age += 1
+	if age <= kid_phase:
+		_stats_calculate()
+	_dump_stats()
+	if age == kid_phase:
+		if genetics.sex.first.sry_gene_power > 0:
+			sprite.texture = genetics.sex.first.adult_texture
+		else:
+			sprite.texture = genetics.sex.second.adult_texture
+	
+func _dump_stats():
+	print(sex," age:",age,"/",lifespan," kid_phase:",kid_phase," strength:",strength,"/",max_strength," active_energy:",active_energy,"/",max_active_energy," passive_energy:",passive_energy,"/",max_passive_energy," energy_drain_rate:",energy_drain_rate," base_speed:",base_speed)
+
+#func _
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
