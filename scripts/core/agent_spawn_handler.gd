f2258cd25189eec343de115fa8715455eb61892c
nookaeo <jeansock.curing183@passinbox.com>
2026-04-19 10:54:31 +0700

    feat: add agent spawner

--- scripts/core/agent_spawn_handler.gd
+++ scripts/core/agent_spawn_handler.gd
@@ -1,11 +1,75 @@
 extends Node
+@export var local_seed :int = 0
+@export var tilemap :TileMapLayer
+@export var agents_directory :Node
+@onready var world_setting :WorldSetting = ($"../..").world_setting
+@onready var agent_spawn :int = world_setting.villagent_spawn
+var world_seed :int = CoreVariable.world_seed
+var rng = RandomNumberGenerator.new()
+var agent_scene = preload("res://scenes/entities/agents/villagents/villagent_base.tscn")
 
+var x_sex_chromosomes :Array = [
+load("res://resources/agent/genetics/sex_chromosomes/x_chromosomes/x_default.tres"),
+]
 
-# Called when the node enters the scene tree for the first time.
+var y_sex_chromosomes :Array = [
+load("res://resources/agent/genetics/sex_chromosomes/y_chromosomes/y_default.tres"),
+]
+
 func _ready() -> void:
-	pass # Replace with function body.
+	rng.seed = world_seed + local_seed
+	var placable_tiles :Array  = get_placable_tiles(tilemap)
+	var random_sex :int
+	
+	for i in range(agent_spawn):
+		var agent :Villagent = agent_scene.instantiate()
+		random_sex = rng.randi_range(0,1)
+		agent.genetics.sex.first = _seeded_pick(x_sex_chromosomes)
+		if random_sex == 0:
+			agent.genetics.sex.second = _seeded_pick(y_sex_chromosomes)
+		elif random_sex == 1:
+			agent.genetics.sex.second = _seeded_pick(x_sex_chromosomes)
+		agent.age += 576
+		agent.tile_map = tilemap
+		agents_directory.add_child(agent)
+		_place_agent(agent,placable_tiles)
+	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+	
+
+func _place_agent(_agent,array :Array) -> void:
+	var pos = _seeded_pick(array)
+	_agent.position = tilemap.map_to_local(pos)
+	array.erase(pos)
+	
+func _seeded_pick(array: Array):
+	if array.is_empty(): 
+		return 
+	return array[rng.randi() % array.size()]
+	
+func get_placable_tiles(map :TileMapLayer) -> Array:
+	var placable :Array = []
+	for pos in map.get_used_cells():
+		var data :TileData = map.get_cell_tile_data(pos)
+		if not data.get_custom_data("grass"):
+			continue
+		placable.append(pos)
+	return placable
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
--- scripts/global_signal/core_signal.gd
+++ scripts/global_signal/core_signal.gd
@@ -1,3 +1,4 @@
 extends Node
 signal day_pass
 signal birth (agent :Node, life_span)
+signal get_time_simulation_day
