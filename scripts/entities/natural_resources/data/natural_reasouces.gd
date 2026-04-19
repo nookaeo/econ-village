0345cc6ee776902d7bdb8484eae642118b39fadf
nookaeo <jeansock.curing183@passinbox.com>
2026-04-19 11:00:01 +0700

    feat: add natural natural resources base

--- scripts/entities/natural_resources/data/natural_reasouces.gd
+++ scripts/entities/natural_resources/data/natural_reasouces.gd
@@ -1,6 +1,7 @@
 extends Resource
 class_name NaturalResources
-
-@export var item :Resource
+@export var name :String
+@export var tool_type :ItemData.ToolType
+@export var item_id :int
 @export var drop_amount :int = 0
 @export var gather_time :float = 0.0
---
+++ scripts/entities/natural_resources/trees/natural_resource_base_script.gd
@@ -0,0 +1,4 @@
+extends StaticBody2D
+@export var natural_resource_data :NaturalResources
+var gatherer :Node
+var gather_time :float
