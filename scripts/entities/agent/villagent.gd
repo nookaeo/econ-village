510ce6dc1dea2eae644e854c0d71ba19890d0816
nookaeo <jeansock.curing183@passinbox.com>
2026-04-19 11:09:58 +0700

    feat: update villagent

--- scripts/entities/agent/villagent.gd
+++ scripts/entities/agent/villagent.gd
@@ -2,23 +2,21 @@ extends CharacterBody2D
 class_name Villagent
 
 const rate :float = 1.0
+@onready var sprite :Sprite2D = %Sprite
+@onready var time_simulation_day :float = CoreVariable.time_simulation_day
 
+#Base variables
 @export var tile_map :TileMapLayer
 @export var genetics :Genetics
 @export var passive_energy_multiplier :float = 10
 @export var energy_drain_multiplier :float = 0.5
-@onready var sprite :Sprite2D = %Sprite
-
-
 var sex :String
 var lifespan :int
 var kid_phase :float
 var max_strength :float 
 var max_active_energy :float
 var max_passive_energy :float
-
-
-var sum_sry_gene_power
+var sum_sry_gene_power :float
 
 var age :int = 0
 var strength :float = 0.0
@@ -26,11 +24,16 @@ var active_energy :float = 0.0
 var passive_energy :float = 0.0
 var energy_drain_rate :float = 0.0
 var base_speed :float = 0.0
+var weight :float = 0.0
 
 var target_position :Vector2 = Vector2.ZERO
 var is_moving: bool = false
+#Item variables
+var inventory :Dictionary
+var tools :Dictionary = {}
 
 #//////////////////////////////////////////////////////////////////////////////#
+
 func _ready() -> void:
 	var current_tile := tile_map.local_to_map(global_position)
 	global_position = tile_map.map_to_local(current_tile)
@@ -39,10 +42,12 @@ func _ready() -> void:
 	CoreSignal.day_pass.connect(_aging)
 	_base_stats_init()
 	_stats_calculate()
-	_dump_stats()
-	
-func _process(_delta: float) -> void:
-	pass
+	set_energy()
+	_change_texture()
+	#_dump_stats()
+
+func _process(delta: float) -> void:
+	_drain_passive_energy(delta)
 	
 func _physics_process(delta :float) -> void:
 	if is_moving:
@@ -66,15 +71,15 @@ func _base_stats_init() -> void:
 	if genetics.sex.first.sry_gene_power + genetics.sex.second.sry_gene_power > 0 :
 		status_average_index = 1
 		sex = "male"
-		if genetics.sex.first.sry_gene_power > 0:
-			sprite.texture = genetics.sex.first.kid_texture
-		else:
+		if genetics.sex.second.sry_gene_power > 0:
 			sprite.texture = genetics.sex.second.kid_texture
+		else:
+			sprite.texture = genetics.sex.first.kid_texture
 	else:
 		status_average_index = 0.5
 		sex = "female"
 		sprite.texture = genetics.sex.first.kid_texture
-		
+	add_to_group(sex)
 	lifespan = int(base_lifespan * (rate - (sum_sry_gene_power * 0.25)) * status_average_index)
 	kid_phase = int(base_kid_phase * status_average_index)
 	max_strength =sum_strength_base * (rate + sum_sry_gene_power) * status_average_index
@@ -102,7 +107,10 @@ func _aging() -> void:
 	age += 1
 	if age <= kid_phase:
 		_stats_calculate()
-	_dump_stats()
+	#_dump_stats()
+	_change_texture()
+	
+func _change_texture():
 	if age == kid_phase:
 		if genetics.sex.first.sry_gene_power > 0:
 			sprite.texture = genetics.sex.first.adult_texture
@@ -112,9 +120,17 @@ func _aging() -> void:
 func _dump_stats():
 	print(sex," age:",age,"/",lifespan," kid_phase:",kid_phase," strength:",strength,"/",max_strength," active_energy:",active_energy,"/",max_active_energy," passive_energy:",passive_energy,"/",max_passive_energy," energy_drain_rate:",energy_drain_rate," base_speed:",base_speed)
 
-#func _
+
+func _drain_passive_energy(_delta :float) -> void:
+	var enrgy_drain :float = (energy_drain_rate * _delta) / time_simulation_day
+	if passive_energy >= enrgy_drain:
+		passive_energy -= enrgy_drain
+	else :
+		die()
 	
-	
+func set_energy() -> void:
+	passive_energy = max_passive_energy
+	active_energy = max_active_energy
 	
 	
 	
