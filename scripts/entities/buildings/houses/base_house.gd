f8efa509d4d7cfdd8e807c2cffe79c690166564c
nookaeo <jeansock.curing183@passinbox.com>
2026-04-21 17:04:15 +0700

    feat: add new house data

---
+++ scripts/entities/buildings/houses/base_house.gd
@@ -0,0 +1,14 @@
+extends Node2D
+class_name House
+@export var house_data :BaseHouseData
+@export var house_maintainance :Dictionary
+var house_owner :Villagent
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
---
+++ scripts/entities/buildings/houses/data/base_house_data.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name BaseHouseData
+
+@export var house_name :String = ""
+@export var texture :Texture2D
+@export var storage_size :float = 100.0
+@export var building_time :float = 5.0
+@export var capacity :int = 5 
+@export var house_maintainance :Dictionary
