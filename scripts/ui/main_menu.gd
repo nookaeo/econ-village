80670d7b2bb92a33e03ec8bb19121662a492d2a4
nookaeo <jeansock.curing183@passinbox.com>
2026-04-21 17:10:16 +0700

    ui: add progress bar for hours in a day

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -2,6 +2,7 @@ extends Panel
 var day :int = 1
 
 func _ready() -> void:
+	%ProgressBar.max_value = CoreConstant.time_simulation_day
 	self.visible = false
 	_stats_update()
 	%MenuButton.pressed.connect(func open_mennu():
@@ -15,6 +16,9 @@ func _process(_delta: float) -> void:
 	
 func _second_show(second :float) -> void:
 	%TimeSecond.text = str(int(second)," s")
+	var sim_hours :int = int(second) % CoreConstant.time_simulation_day
+	%ProgressBar.value = sim_hours
+	%SimHours.text = str(sim_hours)
 	
 func _stats_update() -> void:
 	await get_tree().create_timer(0.1).timeout
