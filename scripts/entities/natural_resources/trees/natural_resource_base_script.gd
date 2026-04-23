0385d4d56e16e89edb203c71bf83feb5aed06775
nookaeo <jeansock.curing183@passinbox.com>
2026-04-23 17:12:51 +0700

    feat: add gathering system

--- scripts/entities/natural_resources/trees/natural_resource_base_script.gd
+++ scripts/entities/natural_resources/trees/natural_resource_base_script.gd
@@ -3,17 +3,31 @@ class_name NaturalResource
 @export var natural_resource_data :NaturalResources
 var tile_map :TileMapLayer
 var biome :String
-var gatherer :Node
-var gather_time :float
-
+var gatherer :Villagent = null
+var gather_progress :float
+@onready var drop_amount :int =  natural_resource_data.drop_amount
 
 
 #/////////////////////////////////////////////////////////
 func _ready() -> void:
-	
+	pass
+func _process(_delta: float) -> void:
 	pass
 #/////////////////////////////////////////////////////////
 
 func gather() -> bool:
-	pass
-	return false
+	var delta = get_physics_process_delta_time()
+	gather_progress += 1.0 * delta
+	gatherer.active_energy -= natural_resource_data.gather_hardness * delta
+	if gather_progress < natural_resource_data.gather_time:
+		return false
+	if drop_amount == 0 :
+		return false
+	NatResourceManager.unregister_resource(self)
+	TilesManager.tiles[biome][tile_map.local_to_map(global_position)] = true
+	if not gatherer.inventory.has(natural_resource_data.item_id):
+		gatherer.inventory[natural_resource_data.item_id] = 0
+	gatherer.inventory[natural_resource_data.item_id] += drop_amount
+	drop_amount = 0
+	self.queue_free()
+	return true
