e647b17fcab2ac159fa6421ed0b29b1ae1b954b0
nookaeo <jeansock.curing183@passinbox.com>
2026-04-24 18:20:57 +0700

    feat: add create house signal

--- scripts/global_signal/core_signal.gd
+++ scripts/global_signal/core_signal.gd
@@ -1,5 +1,6 @@
 extends Node
 signal day_pass
 signal current_second (second :float)
-signal birth (agent :Node, life_span)
+signal birth (agent :Villagent, life_span :int)
 signal get_time_simulation_day
+signal create_house (agent :Villagent)
