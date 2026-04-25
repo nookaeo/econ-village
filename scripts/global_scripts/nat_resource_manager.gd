cdb25d83ccde158c3a725271c0f79595cd987701
nookaeo <jeansock.curing183@passinbox.com>
2026-04-25 22:23:27 +0700

    refactor: clear unnecessaries code

--- scripts/global_scripts/nat_resource_manager.gd
+++ scripts/global_scripts/nat_resource_manager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var bucket_size = 320
+var bucket_size :float = 320
 var grid = {}
 
 func register_resource(node :Node2D):
@@ -8,7 +8,7 @@ func register_resource(node :Node2D):
 	if not grid.has(bucket_pos):
 		grid[bucket_pos] = []
 	grid[bucket_pos].append(node)
-	
+
 func unregister_resource(node :Node2D):
 	var bucket_pos = Vector2i(node.global_position / bucket_size)
 	if grid.has(bucket_pos):
@@ -29,16 +29,19 @@ func get_nearby_resources(search_pos :Vector2) -> Array:
 	return nearby_nodes
 	
 func find_nearest_resource(agent: Node2D,resource_name :String):
-	var raw_list = NatResourceManager.get_nearby_resources(agent.global_position).duplicate()
+	var all_resource = get_tree().get_nodes_in_group(resource_name)
+	if all_resource.size() == 0 :
+			return
+	var raw_list = NatResourceManager.get_nearby_resources(agent.home.global_position).duplicate()
 	var candidates = raw_list.filter(func(n): 
 		return is_instance_valid(n) and not n.is_queued_for_deletion()
 	)
 	var nearest_node = null
-	var min_dist = INF 
-	var all_resource = get_tree().get_nodes_in_group(resource_name)
-	if all_resource.size() == 0 :
-		return
+	var min_dist_1 = INF 
+	var min_dist_2 = INF 
+	
 	for node :NaturalResource in candidates:
+		
 		if not is_instance_valid(node) or node.is_queued_for_deletion():
 			continue
 		if node.natural_resource_data.name != resource_name:
@@ -46,18 +49,19 @@ func find_nearest_resource(agent: Node2D,resource_name :String):
 		if node.gatherer != null:
 			continue
 		var dist = agent.global_position.distance_squared_to(node.global_position)
-		if dist < min_dist:
-			min_dist = dist
+		if dist < min_dist_1:
+			min_dist_1 = dist
 			nearest_node = node
 	
-	if all_resource.size() > 0 and nearest_node == null:
+	if nearest_node == null:
 		for node :NaturalResource in all_resource:
 			var dist = agent.global_position.distance_squared_to(node.global_position)
 			if not is_instance_valid(node) or node.is_queued_for_deletion():
 				continue
 			if node.gatherer != null:
 				continue
-			if dist < min_dist:
-				min_dist = dist
+			if dist < min_dist_2:
+				min_dist_2 = dist
 				nearest_node = node
+		
 	return nearest_node
